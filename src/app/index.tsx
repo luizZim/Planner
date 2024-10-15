7387bcbbac5befc61def92fabaa309637e3d14a8
luiz <mollgoetten3012lpg@gmail.com>
2024-10-15 11:17:02 -0300

    first upload input component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,27 @@
-import { View, Text} from "react-native"
+import { View, Text, Image} from "react-native"
+
+import { Input } from "@/components/input"
 
 
 export default function Index(){
   return(
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-zinc-400">
-        Convide seus amigos e planeje sua próxima viagem!
+    <View className="flex-1 items-center justify-center px-5">
+      <Image 
+        source={require("@/assets/logo.png")}  
+        className="h-8" 
+        resizeMode="contain"
+      />
+
+      <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
+        Convide seus amigos e planeje sua{"\n"} próxima viagem!
       </Text>
+
+      <View className="w-full bg-zinc-900 p-4 rounded-lg my-8 border border-zinc-800">
+        <Input variant="primary">
+          <Input.Field placeholder="Para Onde?"/>
+        </Input>
+      </View>
+      
     </View>
   )
 }
