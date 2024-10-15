105bcc00fee6ba62f22c7bccbe74c71f1d1a8548
luiz <Mollgoetten3012lpg@gmail.com>
2024-10-15 08:45:58 -0300

    upload planner structure and settings

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text} from "react-native"
+
+
+export default function Index(){
+  return(
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-zinc-400">
+        Convide seus amigos e planeje sua próxima viagem!
+      </Text>
+    </View>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+//importação dos tokens para customizar o tema.
+
+import {colors} from "./src/styles/colors";
+import {fontFamily} from "./src/styles/fontFamily";
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors,
+      fontFamily
+    },
+  },
+  plugins: [],
+}
